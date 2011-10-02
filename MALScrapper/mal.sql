c6e4c693a7012e2498931adfd6399ec1fe46d6ac
vosen <vosen@vosen.pl>
2011-10-02 17:19:40 +0200

    Update mapper sql creation code.

--- MALScrapper/mal.sql
+++ MALScrapper/mal.sql
@@ -1,3 +1,14 @@
-﻿CREATE TABLE [Users] (
+﻿CREATE TABLE [Seen] (
+  [Watchlist_Id] INTEGER NOT NULL CONSTRAINT [Seen_Watchlist] REFERENCES [Watchlist]([Id]), 
+  [Anime_Id] INTEGER NOT NULL, 
+  [Score] TINYINT NOT NULL);
+
+
+CREATE TABLE [Watchlist] (
+  [Id] INTEGER NOT NULL PRIMARY KEY);
+
+
+CREATE TABLE [Users] (
   [Id] INTEGER NOT NULL PRIMARY KEY, 
-  [Name] TEXT );
+  [Name] TEXT, 
+  [Watchlist_Id] INTEGER CONSTRAINT [User_Watchlist] REFERENCES [Watchlist]([Id]));
