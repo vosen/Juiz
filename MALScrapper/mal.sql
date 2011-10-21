74e475199e2b090223803044a8cc996465fa4945
vosen <vosen@vosen.pl>
2011-10-21 02:24:39 +0200

    Improve sql Postgres compability and correctness.

--- MALScrapper/mal.sql
+++ MALScrapper/mal.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [Seen] (
-  [Watchlist_Id] INTEGER NOT NULL CONSTRAINT [Seen_Watchlist] REFERENCES [Watchlist]([Id]), 
-  [Anime_Id] INTEGER NOT NULL, 
-  [Score] TINYINT NOT NULL);
-
-
-CREATE TABLE [Watchlist] (
+﻿CREATE TABLE [Watchlist] (
   [Id] INTEGER NOT NULL PRIMARY KEY);
+
+
+CREATE TABLE [Seen] (
+  [Watchlist_Id] INTEGER NOT NULL CONSTRAINT [Seen_Watchlist] REFERENCES [Watchlist]([Id]) ON DELETE CASCADE, 
+  [Anime_Id] INTEGER NOT NULL, 
+  [Score] SMALLINT NOT NULL, 
+  CONSTRAINT [Score_Correct_Rage] CHECK(Score >=0 AND Score <=10));
 
 
 CREATE TABLE [Users] (
@@ -13,4 +14,6 @@ CREATE TABLE [Users] (
   [Name] TEXT, 
   [Watchlist_Id] INTEGER);
 
-  CREATE UNIQUE INDEX [Name_Unique] ON [Users] ([Name]);
+CREATE UNIQUE INDEX [Name_Unique] ON [Users] ([Name]);
+
+
