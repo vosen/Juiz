8601bf5e481619fc0cf8e6d8c4058351b322dc8e
vosen <vosen@vosen.pl>
2012-02-16 23:25:19 +0100

    Change mal db schema to postgres-compatible one.

--- MAL.Mapper/mal.sql
+++ MAL.Mapper/mal.sql
@@ -1,15 +1,19 @@
-﻿CREATE TABLE [Users] (
-  [Id] INTEGER NOT NULL PRIMARY KEY, 
-  [Name] TEXT NULL, 
+﻿BEGIN TRANSACTION;
+CREATE TABLE "Users" (
+  "Id" INTEGER NOT NULL PRIMARY KEY, 
+  "Name" TEXT NULL, 
   -- 0 - not queried / got error, 1 - queried and succeeded
-  [Result] BOOLEAN NOT NULL DEFAULT (0));
+  "Result" BOOLEAN NOT NULL DEFAULT '0'
+);
 
-CREATE UNIQUE INDEX [Name_Unique] ON [Users] ([Name]);
+CREATE UNIQUE INDEX "Name_Unique" ON "Users" ("Name");
 
 
-CREATE TABLE [Seen] (
-  [Anime_Id] INTEGER NOT NULL, 
-  [Score] SMALLINT NOT NULL, 
-  [User_Id] INTEGER NOT NULL CONSTRAINT [User_Id] REFERENCES [Users]([Id]), 
-  CONSTRAINT [Score_Correct_Range] CHECK(Score >=0 AND Score <=10), 
-  CONSTRAINT [] PRIMARY KEY ([Anime_Id], [User_Id]));
+CREATE TABLE "Seen" (
+  "Anime_Id" INTEGER NOT NULL, 
+  "Score" SMALLINT NOT NULL, 
+  "User_Id" INTEGER NOT NULL CONSTRAINT "User_Id" REFERENCES "Users"("Id"), 
+  CONSTRAINT "Score_Correct_Range" CHECK( "Score" >=0 AND "Score" <=10), 
+  CONSTRAINT "pk_Seen" PRIMARY KEY ("Anime_Id", "User_Id")
+);
+COMMIT TRANSACTION;
